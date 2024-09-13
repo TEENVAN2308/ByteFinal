ba93eaafb99546310ffaecc79a186309455a366f
TEENVAN2308 <111627861+TEENVAN2308@users.noreply.github.com>
2024-09-13 11:54:39 +0530

    ewkdsfjldkfs

--- src/pages/round-one/round1end.jsx
+++ src/pages/round-one/round1end.jsx
@@ -7,7 +7,7 @@ function Round1result() {
     const navigate = useNavigate();
     const [secondsRemaining, setSecondsRemaining] = useState(() => {
         const storedTime = window.localStorage.getItem("round1ResultTimer");
-        return storedTime ? JSON.parse(storedTime) : 1200;
+        return storedTime ? JSON.parse(storedTime) : 900;
     });
 
     const [isTimerRunning, setIsTimerRunning] = useState(true);
